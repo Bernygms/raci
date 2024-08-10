d89f1af469132d657fe9f654e6a5cf88385268c7
Bernardino Games <44927900+Bernygms@users.noreply.github.com>
2024-08-09 18:55:02 -0600

    subiendo v1 proyecto raci

---
+++ src/main/java/insus/raci/ProyectoInsusApplication.java
@@ -0,0 +1,11 @@
+package insus.raci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoInsusApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoInsusApplication.class, args);
+	}
+}
---
+++ src/test/java/insus/raci/ProyectoInsusApplicationTests.java
@@ -0,0 +1,13 @@
+package insus.raci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoInsusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
